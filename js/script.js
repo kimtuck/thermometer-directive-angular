890af9912deff8c0d569dbebecc90ac4c2572fa8
rdh <robert.hanson@solutiondesign.com>
2017-03-12 15:05:17 -0500

    Can specify format, intervals, range.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-var app = angular.module('app',[]);
+var app = angular.module('thermo',[]).directive('thermo', ['$window', "$interpolate", function($window, $interpolate) {
 
-app.directive('tgThermometerVertical', ['$window', function($window) {
-	
 	return {
 		restrict: 'E',
 		scope: true,
-		templateUrl: 'js/tg-thermometer-vertical.html',
+		templateUrl: 'vendor/thermometer-directive-angular/js/tg-thermometer-vertical.html',
 		link: function(scope, elem, attrs) {
 
-			scope.size = attrs.size;
-			scope.height = attrs.height;
-			scope.percent = attrs.percent;
+			scope.max=attrs.max || 100;
+			scope.steps = attrs.steps || 4;
+			scope.format = (attrs.format || "{{val}}%").replace(/\[/g,'{').replace(/\]/g,'}');
+            scope.size = attrs.size;
+            scope.height = attrs.height;
+            scope.value = attrs.value || 0;
 
-			console.log(scope);
+            var intervals=[];
+
+            scope.valstr = $interpolate(scope.format)({ val: scope.value} );
+            scope.percent = scope.value / scope.max * 100;
+
+            for (var step = 0; step <= scope.steps; step++) {
+            	var val = (scope.max / scope.steps) * step;
+            	var percent = (val / scope.max)*100;
+            	var interval = { percent: percent, label: $interpolate(scope.format)({ val: val })};
+				intervals.push(interval);
+            }
+            scope.intervals = intervals;
 		}
 	};
 
